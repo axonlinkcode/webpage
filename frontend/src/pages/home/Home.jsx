ac0e2b7b1fc13e9076185cf186a4df393c6c3b46
ozi <ozisadiqkasai@gmail.com>
2025-05-30 11:12:07 +0100

    google form testing

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -3,7 +3,18 @@ import Hero from "../../components/hero/Hero";
 const Home = () => {
   return (
     <div >
-        <Hero />
+      <Hero />
+      <a
+        href="https://docs.google.com/forms/d/e/1FAIpQLScT3WtfRbrUykC2A6oB5wiA9YUx89-_3qfHDnZLLVUBDrW8AA/viewform?usp=dialog"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-600 underline"
+        title="Research Form"
+        style={{ borderBottom: "1px solid green", fontSize:"2rem" }}
+      >
+            Fill out the form
+      </a>
+
     </div>
   );
 };
