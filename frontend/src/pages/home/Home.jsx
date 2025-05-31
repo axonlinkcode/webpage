c1dc7a8c33529f0d818ad99617c63f990bc54de2
ozi <ozisadiqkasai@gmail.com>
2025-05-31 07:24:39 +0100

    revert

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,16 +4,7 @@ const Home = () => {
   return (
     <div >
       <Hero />
-      <a
-        href="https://docs.google.com/forms/d/e/1FAIpQLScT3WtfRbrUykC2A6oB5wiA9YUx89-_3qfHDnZLLVUBDrW8AA/viewform?usp=dialog"
-        target="_blank"
-        rel="noopener noreferrer"
-        className="text-blue-600 underline"
-        title="Research Form"
-        style={{ borderBottom: "1px solid green", fontSize:"2rem" }}
-      >
-            Fill out the form
-      </a>
+      
 
     </div>
   );
