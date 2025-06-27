7eaa3ba2155ff906acc445485d4a05918806ba13
Ozi <ozisadiqkasai@gmail.com>
2025-06-27 01:50:17 +0100

    rebase

--- frontend/src/components/buttons/Button.jsx
+++ frontend/src/components/buttons/Button.jsx
@@ -3,10 +3,6 @@ import './button.css'
 const Button = ({ name, link,className }) => {
     return (
         <a className={`button ${className}`} href={link}>{name}</a>
-<<<<<<< HEAD
-=======
-
->>>>>>> b69281c (query card animation)
     )
 }
 
