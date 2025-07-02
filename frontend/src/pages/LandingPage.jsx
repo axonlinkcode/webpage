1ecfa4fad173728d98ff374a60f95f8331deebb2
Ozi <ozisadiqkasai@gmail.com>
2025-07-02 20:13:58 +0100

    animation added

--- frontend/src/pages/LandingPage.jsx
+++ frontend/src/pages/LandingPage.jsx
@@ -1,17 +1,29 @@
 import Home from './home/Home';
-import Services from '../components/services/Services'
+import Services from '../components/services/Services';
 import Products from '../components/products/Products';
 import Team from '../components/team/Team';
+import FadeInOnScroll from '../components/FadeInOnScroll';
 
 const LandingPage = () => {
   return (
-    <div>
-      <Home />
-      <Services />
-      <Products />
-      <Team />
-    </div>
-  )
-}
+    <>
+      <FadeInOnScroll>
+        <Home />
+      </FadeInOnScroll>
 
-export default LandingPage
+      <FadeInOnScroll delay={0.1}>
+        <Services />
+      </FadeInOnScroll>
+
+      <FadeInOnScroll delay={0.2}>
+        <Products />
+      </FadeInOnScroll>
+
+      <FadeInOnScroll delay={0.3}>
+        <Team />
+      </FadeInOnScroll>
+    </>
+  );
+};
+
+export default LandingPage;
