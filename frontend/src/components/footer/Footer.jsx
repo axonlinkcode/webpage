505096d92e52089730b07ccb3b3f99522bdc815d
Ozi <ozisadiqkasai@gmail.com>
2025-07-02 11:05:43 +0100

    footer small phones done

--- frontend/src/components/footer/Footer.jsx
+++ frontend/src/components/footer/Footer.jsx
@@ -3,21 +3,19 @@ import { assets } from "../../assets/assets";
 const Footer = () => {
     return (
         <footer className="footer">
-            <ul className='footer__top-list'>
+            <ul className='footer__top'>
                 <li>
-                    <div>
-                        <a href="#home">Home</a>
-                        <a href="#services">Services</a>
-                        <a href="#products">Products</a>
-                        <a href="#team">Team</a>
-                    </div>
+                    <a href="#home">Home</a>
+                    <a href="#services">Services</a>
+                    <a href="#products">Products</a>
+                    <a href="#team">Team</a>
                 </li>
-                <li>
+                <li className='footer__logo' >
                     <img src={assets.logowhite} alt="logo" />
                 </li>
             </ul>
-            <div className="footer__bottom">
-                 <div className='footer__bottom-social'>
+            <ul className="footer__bottom">
+                <li className='footer__bottom-social'>
                     <a href="#"
                         type='email'>
                         axonlink@axonlink.ai
@@ -28,29 +26,14 @@ const Footer = () => {
                     <a href="#">
                         <img src={assets.x} alt="" />
                     </a>
-                </div>
-                <div className="footer__bottom-left">
-                    <a>© 2025 axonlink. All rights reserved</a>
-                    <a href="#">Privacy Policy</a>
-                    <a href="#">Terms of Service</a>
-                </div>
-                <div className="footer__bottom-right">
-                    {/* <a href="#">Privacy Policy</a>
-                    <a href="#">Terms of Service</a> */}
-                </div>
-                {/* <div className='footer__bottom-social'>
-                    <a href="#"
-                        type='email'>
-                        axonlink@axonlink.ai
-                    </a>
-                    <a href="#">
-                        <img src={assets.linkedin} alt="" />
-                    </a>
-                    <a href="#">
-                        <img src={assets.x} alt="" />
-                    </a>
-                </div> */}
-            </div>
+                </li>
+            </ul>
+            <ul className="footer__bottom-policy">
+                <a>© 2025 axonlink. All rights reserved</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Terms of Service</a>
+            </ul>
+
         </footer>
     );
 };
